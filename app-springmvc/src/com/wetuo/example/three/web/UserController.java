1d27043914443d934d04cd6dfdafc242995bb1b2
Administrator <Administrator@USER-20130226GL>
2014-03-05 06:57:33 +0800

    springmvc

---
+++ app-springmvc/src/com/wetuo/example/three/web/UserController.java
@@ -0,0 +1,20 @@
+package com.wetuo.example.three.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class UserController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest arg0,
+			HttpServletResponse arg1) throws Exception {
+
+		ModelAndView mv = new ModelAndView("index");
+		mv.addObject("username", " arac ");
+		return mv;
+	}
+
+}
